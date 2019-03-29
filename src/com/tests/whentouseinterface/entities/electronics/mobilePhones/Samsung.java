f6dc488b20eb877102daaeba9616fbd5e3e333c8
flavio10araujo <flavio10araujo@yahoo.com.br>
2019-03-29 14:06:23 -0400

    Update Samsung.java

--- src/com/tests/whentouseinterface/entities/electronics/mobilePhones/Samsung.java
+++ src/com/tests/whentouseinterface/entities/electronics/mobilePhones/Samsung.java
@@ -1,7 +1,8 @@
 package com.tests.whentouseinterface.entities.electronics.mobilePhones;
 
 import com.tests.whentouseinterface.entities.electronics.MobilePhone;
+import com.tests.whentouseinterface.interfaces.ISamsung;
 
-public class Samsung extends MobilePhone {
+public class Samsung extends MobilePhone implements ISamsung {
 
 }
