24049c3b847c21b7c82d442171b44f96adbd5ec3
flavio10araujo <flavio10araujo@yahoo.com.br>
2019-03-29 14:04:52 -0400

    Add files via upload

--- src/com/tests/whentouseinterface/entities/electronics/tvs/Samsung.java
+++ src/com/tests/whentouseinterface/entities/electronics/tvs/Samsung.java
@@ -1,7 +1,8 @@
 package com.tests.whentouseinterface.entities.electronics.tvs;
 
 import com.tests.whentouseinterface.entities.electronics.TV;
+import com.tests.whentouseinterface.interfaces.ISamsung;
 
-public class Samsung extends TV {
+public class Samsung extends TV implements ISamsung {
 
 }
